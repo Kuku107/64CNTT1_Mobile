bb3b60ff0a1869889b6e3d46f8dee5415100f652
TRINH DUC DAI <ducdai@Duc-Dais-MacBook-Air.local>
2025-03-04 08:53:11 +0700

    4Mar via Terminal

---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.simpleasynctask;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+        new SimpleAsyncTask(mTextView).execute();
+    }
+}
