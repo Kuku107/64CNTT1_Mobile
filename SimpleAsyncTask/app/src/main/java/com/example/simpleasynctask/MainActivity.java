6bd37fc41e56fba4adee01ab929265aac2681a08
TRINH DUC DAI <ducdai@Duc-Dais-MacBook-Air.local>
2025-03-11 09:02:36 +0700

    11Mar25

--- SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -11,14 +11,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
+    private TextView mTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.textView1);
+        mTextView = (TextView) findViewById(R.id.textView1);
+        if (savedInstanceState != null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -29,5 +33,11 @@ public class MainActivity extends AppCompatActivity {
     public void startTask(View view) {
         mTextView.setText(R.string.napping);
         new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
     }
 }
