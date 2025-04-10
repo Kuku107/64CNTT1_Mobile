e721f43b1507416c5d6eb2470cc22342aed6ce0d
ducnguyentrung24 <2251061745@e.tlu.edu.vn>
2025-04-10 14:30:39 +0700

    remove redundant imports

--- YourClassroom/app/src/main/java/android/example/yourclassroom/controller/AttachmentAdapter.java
+++ YourClassroom/app/src/main/java/android/example/yourclassroom/controller/AttachmentAdapter.java
@@ -3,19 +3,14 @@ package android.example.yourclassroom.controller;
 import android.content.Context;
 import android.example.yourclassroom.R;
 import android.example.yourclassroom.model.Attachment;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
